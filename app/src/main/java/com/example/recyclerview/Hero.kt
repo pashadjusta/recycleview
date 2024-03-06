cf48c2e035f640bb825f44c53059ef6e29deb4f8
aurin <pashaadjusta@gmail.com>
2024-03-06 13:13:46 +0700

    done

---
+++ app/src/main/java/com/example/recyclerview/Hero.kt
@@ -0,0 +1,9 @@
+package com.example.recyclerview
+
+class Hero (
+        var name: String = "",
+        var from: String = "",
+        var photo: String = ""
+
+
+)
---
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -0,0 +1,30 @@
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.ListHeroAdapter
+import com.example.recyclerview.Hero
+import com.example.recyclerview.HeroesData
+import com.example.recyclerview.R
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var rvHeroes: RecyclerView
+    private val list: ArrayList<Hero> = ArrayList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvHeroes = findViewById(R.id.rv_hero)
+        rvHeroes.setHasFixedSize(true)
+
+        list.addAll(listData)
+        showRecyclerList()
+    }
+
+    private fun showRecyclerList() {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListHeroAdapter(list)
+        rvHeroes.adapter = listHeroAdapter
+    }
+}
